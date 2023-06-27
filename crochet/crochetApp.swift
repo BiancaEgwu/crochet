4a6180dfb2a331cadcb33d22da06de0cb2645d24
Bianca Egwu <bianca.egwu9@gmail.com>
2023-06-27 10:28:52 -0400

    Initial Commit

---
+++ crochet/crochetApp.swift
@@ -0,0 +1,17 @@
+//
+//  crochetApp.swift
+//  crochet
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct crochetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
